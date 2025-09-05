f3450ec59d377909fd90842a081196d993e680c4
Aaronkefyalew <159499784+Aaronkefyalew@users.noreply.github.com>
2025-09-06 00:59:57 +1100

    Update Button.tsx

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { ButtonProps } from '../../interfaces';
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
 
